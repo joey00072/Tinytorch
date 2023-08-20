c5a9452c0ef0e23363d84cd44cacd362d53f398b
joey00072 <00shxf@gmail.com>
2023-08-20 13:22:59 +0530

    Added simple backward pass

--- tinytorch.py
+++ tinytorch.py
@@ -1,54 +1,89 @@
 import numpy as np
 
+
 class Tensor:
-    def __init__(self,data):
-        self.data = data if isinstance(data,np.ndarray) else np.array(data)
+    def __init__(self, data):
+        self.data = data if isinstance(data, np.ndarray) else np.array(data)
+        self.grad = None
         self._ctx = None
         
-    def __add__(self,other):
-        fn = Function(Add,self,other)
-        result = Add.forward(self,other)
+
+    def __add__(self, other):
+        fn = Function(Add, self, other)
+        result = Add.forward(self, other)
         result._ctx = fn
         return result
 
-    def __mul__(self,other):
-        fn = Function(Mul,self,other)
-        result = Mul.forward(self,other)
+    def __mul__(self, other):
+        fn = Function(Mul, self, other)
+        result = Mul.forward(self, other)
         result._ctx = fn
         return result
-        
-    
+
     def __repr__(self):
         return f"tensor({self.data})"
-    
+
+    def backward(self,grad=None):
+        if grad is None:
+            grad = Tensor([1.])
+            self.grad = grad
+        
+        op = self._ctx.op
+        child_nodes = self._ctx.args
+        
+        grads = op.backward(self._ctx,grad)
+        
+        for tensor,grad in zip(child_nodes,grads):
+            tensor.grad = grad
+            
+
 class Function:
-    def __init__(self,op,*args):
+    def __init__(self, op, *args):
         self.op = op
-        self.args = args        
+        self.args = args
+
 
 class Add:
     @staticmethod
-    def forward(x,y):
+    def forward(x, y):
         return Tensor(x.data + y.data)
-    
+
     @staticmethod
-    def backward(ctx,grad):
-        x,y = ctx.args
-        return Tensor([1]),Tensor([1])
+    def backward(ctx, grad):
+        x, y = ctx.args
+        return Tensor([1]), Tensor([1])
+
 
 class Mul:
     @staticmethod
-    def forward(x,y):
-        return Tensor(x.data * y.data) # z = x*y
-    
+    def forward(x, y):
+        return Tensor(x.data * y.data)  # z = x*y
+
     @staticmethod
-    def backward(ctx,grad):
-        x,y = ctx.args
-        return  Tensor(y.data), Tensor(x.data) #  dz/dx, dz/dy
-    
+    def backward(ctx, grad):
+        x, y = ctx.args
+        return Tensor(y.data), Tensor(x.data)  #  dz/dx, dz/dy
+
+
 if __name__ == "__main__":
     x = Tensor([8])
-    y = Tensor([5])    
-    z = x*y
+    y = Tensor([5])
+    
+    print("Add")
+    z = x + y
     print(z)
+    z.backward()
+    print(f"x: {x} , grad {x.grad}")
+    print(f"y: {y} , grad {y.grad}")
+    print("="*100)
+    
+    print("Mul")
+    z = x * y
+    print(z)
+    z.backward()
+    print(f"x: {x} , grad {x.grad}")
+    print(f"y: {y} , grad {y.grad}")
+    print("="*100)
+    
+    
     
--- visulize.py
+++ visulize.py
@@ -1,26 +1,28 @@
 import graphviz
 from tinytorch import *
 
-G = graphviz.Digraph(format='png')
+G = graphviz.Digraph(format="png")
 G.clear()
-def visit_nodes(G:graphviz.Digraph,node:Tensor):
+
+
+def visit_nodes(G: graphviz.Digraph, node: Tensor):
     uid = str(id(node))
-    G.node(uid,f"Tensor: {str(node.data) } ")
+    G.node(uid, f"Tensor: {str(node.data) } ")
     if node._ctx:
         ctx_uid = str(id(node._ctx))
-        G.node(ctx_uid,f"Context: {str(node._ctx.op.__name__)}")
-        G.edge(uid,ctx_uid)
+        G.node(ctx_uid, f"Context: {str(node._ctx.op.__name__)}")
+        G.edge(uid, ctx_uid)
         for child in node._ctx.args:
-            G.edge(ctx_uid,str(id(child)))
-            visit_nodes(G,child)
+            G.edge(ctx_uid, str(id(child)))
+            visit_nodes(G, child)
 
 
 if __name__ == "__main__":
     x = Tensor([8])
-    y = Tensor([5])    
-    z = x+y
-    visit_nodes(G,z)
-    G.render(directory="vis",view=True)
+    y = Tensor([5])
+    z = x + y
+    visit_nodes(G, z)
+    G.render(directory="vis", view=True)
     print(z)
-    
+
     print(len(G.body))
