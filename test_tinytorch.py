a7b7d70da0972c4e740c26cb262c730c6047c3b1
joey00072 <shubhamchoudhari00072@gmail.com>
2023-08-31 14:43:07 +0000

    :rocket: IMP ops completed

--- test_tinytorch.py
+++ test_tinytorch.py
@@ -21,10 +21,15 @@ def test_add():
     z_tt = x_tt + y_tt
     z_tt.sum().backward()
 
-    assert np.allclose(z_t.detach().numpy(), z_tt.data), "Addition results do not match between PyTorch and tinytorch."
-    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), "Gradients for x do not match between PyTorch and tinytorch."
-    assert np.allclose(y_t.grad.numpy(), y_tt.grad.data), "Gradients for y do not match between PyTorch and tinytorch."
-
+    assert np.allclose(
+        z_t.detach().numpy(), z_tt.data
+    ), "Addition results do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        x_t.grad.numpy(), x_tt.grad.data
+    ), "Gradients for x do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        y_t.grad.numpy(), y_tt.grad.data
+    ), "Gradients for y do not match between PyTorch and tinytorch."
 
 
 def test_mul():
@@ -43,9 +48,15 @@ def test_mul():
     z_tt = x_tt * y_tt
     z_tt.sum().backward()
 
-    assert np.allclose(z_t.detach().numpy(), z_tt.data), "Multiplication results do not match between PyTorch and tinytorch."
-    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), "Gradients for x do not match between PyTorch and tinytorch."
-    assert np.allclose(y_t.grad.numpy(), y_tt.grad.data), "Gradients for y do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        z_t.detach().numpy(), z_tt.data
+    ), "Multiplication results do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        x_t.grad.numpy(), x_tt.grad.data
+    ), "Gradients for x do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        y_t.grad.numpy(), y_tt.grad.data
+    ), "Gradients for y do not match between PyTorch and tinytorch."
 
 
 def test_div():
@@ -64,9 +75,15 @@ def test_div():
     z_tt = x_tt / y_tt
     z_tt.sum().backward()
 
-    assert np.allclose(z_t.detach().numpy(), z_tt.data), "Division results do not match between PyTorch and tinytorch."
-    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), "Gradients for x do not match between PyTorch and tinytorch."
-    assert np.allclose(y_t.grad.numpy(), y_tt.grad.data), "Gradients for y do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        z_t.detach().numpy(), z_tt.data
+    ), "Division results do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        x_t.grad.numpy(), x_tt.grad.data
+    ), "Gradients for x do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        y_t.grad.numpy(), y_tt.grad.data
+    ), "Gradients for y do not match between PyTorch and tinytorch."
 
 
 def test_sub():
@@ -85,9 +102,16 @@ def test_sub():
     z_tt = x_tt - y_tt
     z_tt.sum().backward()
 
-    assert np.allclose(z_t.detach().numpy(), z_tt.data), "Subtraction results do not match between PyTorch and tinytorch."
-    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), "Gradients for x do not match between PyTorch and tinytorch."
-    assert np.allclose(y_t.grad.numpy(), y_tt.grad.data), "Gradients for y do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        z_t.detach().numpy(), z_tt.data
+    ), "Subtraction results do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        x_t.grad.numpy(), x_tt.grad.data
+    ), "Gradients for x do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        y_t.grad.numpy(), y_tt.grad.data
+    ), "Gradients for y do not match between PyTorch and tinytorch."
+
 
 def test_sum():
     x = np.random.rand(3, 3)
@@ -101,8 +125,13 @@ def test_sum():
     z_tt = x_tt.sum()
     z_tt.backward()
 
-    assert np.allclose(z_t.detach().numpy(), z_tt.data), "Sum results do not match between PyTorch and tinytorch."
-    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), "Gradients do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        z_t.detach().numpy(), z_tt.data
+    ), "Sum results do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        x_t.grad.numpy(), x_tt.grad.data
+    ), "Gradients do not match between PyTorch and tinytorch."
+
 
 def test_sum_axis(axis=1):
     x = np.random.rand(3, 3)
@@ -116,8 +145,13 @@ def test_sum_axis(axis=1):
     z_tt = x_tt.sum(axis=axis)
     z_tt.sum().backward()  # Summing again to get a scalar for backward()
 
-    assert np.allclose(z_t.detach().numpy(), z_tt.data), f"Sum along axis {axis} results do not match between PyTorch and tinytorch."
-    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), f"Gradients along axis {axis} do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        z_t.detach().numpy(), z_tt.data
+    ), f"Sum along axis {axis} results do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        x_t.grad.numpy(), x_tt.grad.data
+    ), f"Gradients along axis {axis} do not match between PyTorch and tinytorch."
+
 
 def test_reshape():
     x = np.random.rand(2, 3, 4)
@@ -132,14 +166,19 @@ def test_reshape():
     z_tt = x_tt.reshape(new_shape)
     z_tt.sum().backward()
 
-    assert np.allclose(z_t.detach().numpy(), z_tt.data), "Reshape results do not match between PyTorch and tinytorch."
-    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), "Gradients do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        z_t.detach().numpy(), z_tt.data
+    ), "Reshape results do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        x_t.grad.numpy(), x_tt.grad.data
+    ), "Gradients do not match between PyTorch and tinytorch."
+
 
 def test_custom_eq():
     # Define custom function f(x, y) within the scope of test_custom_eq
     def f(x, y):
-        return x * x + (x * y) / (x + y) + x*(x+y)
-        
+        return x * x + (x * y) / (x + y) + x * (x + y)
+
     x = np.random.rand(3, 3)
     y = np.random.rand(3, 3)
 
@@ -158,10 +197,154 @@ def test_custom_eq():
     z_tt.sum().backward()
 
     # Assertions to check if both results match
-    assert np.allclose(z_t.detach().numpy(), z_tt.data), "Custom eq results do not match between PyTorch and tinytorch."
-    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), "Gradients for x do not match between PyTorch and tinytorch."
-    assert np.allclose(y_t.grad.numpy(), y_tt.grad.data), "Gradients for y do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        z_t.detach().numpy(), z_tt.data
+    ), "Custom eq results do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        x_t.grad.numpy(), x_tt.grad.data
+    ), "Gradients for x do not match between PyTorch and tinytorch."
+    assert np.allclose(
+        y_t.grad.numpy(), y_tt.grad.data
+    ), "Gradients for y do not match between PyTorch and tinytorch."
 
 
-if __name__  == "__main__":
-    test_add()
+def test_matmul():
+    x = np.random.rand(3, 4)
+    y = np.random.rand(4, 3)
+
+    x_t = torch.tensor(x, requires_grad=True)
+    y_t = torch.tensor(y, requires_grad=True)
+
+    x_tt = tinytorch.tensor(x, requires_grad=True)
+    y_tt = tinytorch.tensor(y, requires_grad=True)
+
+    z_t = x_t @ y_t
+    z_t.sum().backward()
+
+    z_tt = x_tt @ y_tt
+    z_tt.sum().backward()
+
+    # Assertions
+    assert np.allclose(z_t.detach().numpy(), z_tt.data), "Matmul results do not match."
+    assert np.allclose(
+        x_t.grad.numpy(), x_tt.grad.data
+    ), "Gradients for x do not match."
+    assert np.allclose(
+        y_t.grad.numpy(), y_tt.grad.data
+    ), "Gradients for y do not match."
+
+
+def test_pow():
+    x = np.random.rand(3, 3)
+    exponent = 2
+
+    x_t = torch.tensor(x, requires_grad=True)
+    x_tt = tinytorch.tensor(x, requires_grad=True)
+
+    z_t = x_t ** (exponent)
+    z_t.sum().backward()
+
+    z_tt = x_tt ** (exponent)
+    z_tt.sum().backward()
+
+    # Assertions
+    assert np.allclose(z_t.detach().numpy(), z_tt.data), "Pow results do not match."
+    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), "Gradients do not match."
+
+
+def test_tanh():
+    x = np.random.rand(3, 3)
+
+    x_t = torch.tensor(x, requires_grad=True)
+    x_tt = tinytorch.tensor(x, requires_grad=True)
+
+    z_t = torch.tanh(x_t)
+    z_t.sum().backward()
+
+    z_tt = tinytorch.tanh(x_tt)
+    z_tt.sum().backward()
+
+    # Assertions
+    assert np.allclose(z_t.detach().numpy(), z_tt.data), "Tanh results do not match."
+    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), "Gradients do not match."
+
+
+def test_relu():
+    x = np.random.rand(3, 3)
+
+    x_t = torch.tensor(x, requires_grad=True)
+    x_tt = tinytorch.tensor(x, requires_grad=True)
+
+    z_t = torch.relu(x_t)
+    z_t.sum().backward()
+
+    z_tt = tinytorch.relu(x_tt)
+    z_tt.sum().backward()
+
+    # Assertions
+    assert np.allclose(z_t.detach().numpy(), z_tt.data), "ReLU results do not match."
+    assert np.allclose(x_t.grad.numpy(), x_tt.grad.data), "Gradients do not match."
+
+
+def test_mse_loss():
+    y_pred = np.random.rand(2, 2)
+    y_true = np.random.rand(2, 2)
+
+    y_pred_t = torch.tensor(y_pred, requires_grad=True)
+    y_true_t = torch.tensor(y_true, requires_grad=False)
+
+    y_pred_tt = tinytorch.tensor(y_pred, requires_grad=True)
+    y_true_tt = tinytorch.tensor(y_true, requires_grad=False)
+
+    loss_t = torch.nn.functional.mse_loss(y_pred_t, y_true_t)
+    loss_t.backward()
+
+    loss_tt = tinytorch.mse_loss(y_pred_tt, y_true_tt)
+    print(loss_t, loss_tt)
+    loss_tt.backward()
+
+    # Assertions
+    assert np.allclose(
+        loss_t.detach().numpy(), loss_tt.data
+    ), "MSE Loss results do not match."
+    assert np.allclose(
+        y_pred_t.grad.numpy(), y_pred_tt.grad.data
+    ), "Gradients do not match."
+
+
+def test_cross_entropy():
+    # Create random logits and labels
+    logits = np.random.rand(5, 3)  # 5 samples, 3 classes
+    labels = np.random.randint(0, 3, size=(5,))  # 5 samples, labels from 0 to 2
+
+    # Convert to PyTorch tensors
+    logits_t = torch.tensor(logits, requires_grad=True, dtype=torch.float32)
+    labels_t = torch.tensor(labels, requires_grad=False, dtype=torch.long)
+
+    # Convert to tinytorch tensors
+    logits_tt = tinytorch.tensor(logits, requires_grad=True)
+    labels_tt = tinytorch.tensor(labels, requires_grad=False)
+
+    # Compute loss using PyTorch
+    loss_t = torch.nn.functional.cross_entropy(logits_t, labels_t)
+    loss_t.backward()
+
+    # Compute loss using tinytorch (assuming cross_entropy is implemented)
+    loss_tt = tinytorch.cross_entropy(logits_tt, labels_tt)
+    loss_tt.sum().backward()
+
+    # Assertions
+    assert np.allclose(
+        loss_t.detach().numpy(), loss_tt.data, atol=1e-7
+    ), "Cross-entropy Loss results do not match."
+    assert np.allclose(
+        logits_t.grad.numpy(), logits_tt.grad.data, atol=1e-7
+    ), "Gradients do not match."
+
+
+# Add this line to the end of your script to run the test
+if __name__ == "__main__":
+    test_cross_entropy()
+
+if __name__ == "__main__":
+    test_mse_loss()
--- tinytorch.py
+++ tinytorch.py
@@ -50,6 +50,9 @@ class Tensor:
         other = Tensor.ensure_tensor(other)
         return self * other.reciprocal()
 
+    def __matmul__(self, other):
+        return MatMul.apply(self, Tensor.ensure_tensor(other))
+
     def __radd__(self, other) -> Tensor:
         return Add.apply(self, Tensor.ensure_tensor(other))
 
@@ -62,6 +65,9 @@ class Tensor:
     def __rtruediv__(self, other) -> Tensor:
         other = Tensor.ensure_tensor(other)
         return other * self.reciprocal()
+
+    def __rmatmul__(self, other) -> Tensor:
+        return MatMul.apply(Tensor.ensure_tensor(other), self)
 
     def __iadd__(self, other) -> Tensor:
         return Add.apply(self, Tensor.ensure_tensor(other))
@@ -79,19 +85,25 @@ class Tensor:
     def __neg__(self) -> Tensor:
         return -1 * self
 
+    def __pow__(self, other) -> Tensor:
+        return Power.apply(self, Tensor.ensure_tensor(other))
+
+    def __len__(self) -> int:
+        return len(self.data)
+
     def reciprocal(self) -> Tensor:
         return Reciprocal.apply(self)
 
     def exp(self) -> Tensor:
         return Exp.apply(self)
-    
-    def reshape(self,shape):
-        curr = math.prod(self.shape)
-        target = math.prod((s for s in shape if s!=-1))
-        shape = tuple(curr//target if s==-1 else s for s in shape)
-        return Reshape.apply(self,shape)
 
-    def _reduce_shape(self,axis=None):
+    def reshape(self, shape):
+        curr = math.prod(self.shape)
+        target = math.prod((s for s in shape if s != -1))
+        shape = tuple(curr // target if s == -1 else s for s in shape)
+        return Reshape.apply(self, shape)
+
+    def _reduce_shape(self, axis=None):
         # Determine which axes to reduce
         if axis is None:
             reduction_axes = list(range(len(self.shape)))
@@ -99,24 +111,32 @@ class Tensor:
             reduction_axes = [axis]
         else:
             reduction_axes = list(axis)
-        
+
         reduction_axes = [a if a >= 0 else a + len(self.shape) for a in reduction_axes]
-        
-        post_reduction_shape = [self.shape[i] for i in range(len(self.shape)) if i not in reduction_axes]
-        
-        shape_with_kept_dims = tuple(1 if i in reduction_axes else self.shape[i] for i in range(len(self.shape)))
-        
+
+        post_reduction_shape = tuple(
+            self.shape[i] for i in range(len(self.shape)) if i not in reduction_axes
+        )
+
+        shape_with_kept_dims = tuple(
+            1 if i in reduction_axes else self.shape[i] for i in range(len(self.shape))
+        )
+
         return post_reduction_shape, shape_with_kept_dims
 
     def sum(self, axis=None, keepdims=False) -> Tensor:
-        shape , new_shape =  self._reduce_shape(axis)
-        ret:Tensor = Sum.apply(self,new_shape)
+        shape, new_shape = self._reduce_shape(axis)
+        shape = shape if (shape) != () else (1,)
+        ret: Tensor = Sum.apply(self, new_shape)
         return ret if keepdims else ret.reshape(shape=shape)
-  
 
     @property
     def shape(self) -> tuple:
         return self.data.shape
+
+    @property
+    def size(self) -> tuple:
+        return self.data.size
 
     def detach(self) -> Tensor:
         self._ctx = None
@@ -140,6 +160,8 @@ class Tensor:
             return
 
         if grad is None:
+            if self.size != 1:
+                raise RuntimeError("Backward can not be called on non zero tensor")
             grad = Tensor([1.0])
             self.grad = grad
 
@@ -235,6 +257,19 @@ class Reciprocal(Function):
         return Tensor(-1.0 / (x.data**2)) * grad
 
 
+class MatMul(Function):
+    @staticmethod
+    def forward(x, y):
+        return Tensor(np.matmul(x.data, y.data))
+
+    @staticmethod
+    def backward(ctx, grad):
+        x, y = ctx.args
+        grad_x = np.matmul(grad.data, y.data.T)
+        grad_y = np.matmul(x.data.T, grad.data)
+        return Tensor(grad_x), Tensor(grad_y)
+
+
 class Exp(Function):
     @staticmethod
     def forward(x):
@@ -248,25 +283,125 @@ class Exp(Function):
 
 class Reshape(Function):
     @staticmethod
-    def forward(x,shape) -> Tensor:
+    def forward(x, shape) -> Tensor:
         return Tensor(x.data.reshape(tuple(shape)))
-    
+
     @staticmethod
-    def backward(ctx:Function,grad:Tensor) -> Tensor:
+    def backward(ctx: Function, grad: Tensor) -> Tensor:
         print(f"{ctx.args=}")
-        x,_ = ctx.args
-        return Tensor(grad.data.reshape(x.shape)),None
+        x, _ = ctx.args
+        return Tensor(grad.data.reshape(x.shape)), None
 
 
 class Sum(Function):
-    def forward(x:Tensor, new_shape:tuple) -> Tensor:
-      axis = tuple(idx for idx,(a,b) in enumerate(zip(x.shape, new_shape)) if a != b)
-      x = x.data.sum(axis, keepdims=True)
-      return Tensor(x)
-  
-    def backward(ctx, grad:Tensor) -> Tensor:
-      x,_ = ctx.args
-      return Tensor(np.broadcast_to(grad.data, x.shape)) , None
+    def forward(x: Tensor, new_shape: tuple) -> Tensor:
+        axis = tuple(
+            idx for idx, (a, b) in enumerate(zip(x.shape, new_shape)) if a != b
+        )
+        x = x.data.sum(axis, keepdims=True)
+        return Tensor(x)
+
+    def backward(ctx, grad: Tensor) -> Tensor:
+        x, _ = ctx.args
+        return Tensor(np.broadcast_to(grad.data, x.shape)), None
+
+
+class Power(Function):
+    @staticmethod
+    def forward(x, y):
+        return Tensor(np.power(x.data, y.data))
+
+    @staticmethod
+    def backward(ctx, grad):
+        x, y = ctx.args
+        grad_x = y.data * np.power(x.data, y.data - 1) * grad.data
+        return Tensor(grad_x), None
+
+
+class Tanh(Function):
+    @staticmethod
+    def forward(x):
+        return Tensor(np.tanh(x.data))
+
+    @staticmethod
+    def backward(ctx, grad):
+        (x,) = ctx.args
+        grad_tanh = 1 - np.tanh(x.data) ** 2
+        return Tensor(grad_tanh) * grad
+
+
+class ReLU(Function):
+    @staticmethod
+    def forward(x):
+        return Tensor(np.maximum(0, x.data))
+
+    @staticmethod
+    def backward(ctx, grad):
+        (x,) = ctx.args
+        grad_relu = np.where(x.data > 0, 1, 0)
+        return Tensor(grad_relu) * grad
+
+
+class Sin(Function):
+    @staticmethod
+    def forward(x):
+        return Tensor(np.sin(x.data))
+
+    @staticmethod
+    def backward(ctx, grad):
+        (x,) = ctx.args
+        return Tensor(np.cos(x.data)) * grad
+
+
+class CrossEntropy(Function):
+    @staticmethod
+    def forward(y_pred: Tensor, y_true: Tensor) -> Tensor:
+        exps = np.exp(y_pred.data - np.max(y_pred.data, axis=1, keepdims=True))
+        probs = exps / np.sum(exps, axis=1, keepdims=True)
+        log_likelihood = -np.log(
+            probs[np.arange(len(y_true.data)), y_true.data.astype(int)]
+        )
+        return Tensor(np.mean(log_likelihood))
+
+    @staticmethod
+    def backward(ctx: Function, grad: Tensor) -> list[Tensor]:
+        y_pred, y_true = ctx.args
+        exps = np.exp(y_pred.data - np.max(y_pred.data, axis=1, keepdims=True))
+        probs = exps / np.sum(exps, axis=1, keepdims=True)
+        d_loss = np.zeros_like(probs)
+        d_loss[np.arange(len(y_true.data)), y_true.data.astype(int)] -= 1
+        d_loss += probs
+        d_loss /= len(y_true.data)
+        return [Tensor(d_loss) * grad, None]
+
+
+def cross_entropy(y_pred: Tensor, y_true: Tensor) -> Tensor:
+    return CrossEntropy.apply(y_pred, y_true)
+
+
+def exp(x) -> Tensor:
+    return Exp.apply(x)
+
+
+def tanh(x) -> Tensor:
+    return Tanh.apply(x)
+
+
+def relu(x) -> Tensor:
+    return ReLU.apply(x)
+
+
+def sin(x) -> Tensor:
+    return Sin.apply(x)
+
+
+def cos(x) -> Tensor:
+    return sin(x + np.pi)
+
+
+def mse_loss(y_pred: Tensor, y_true: Tensor) -> Tensor:
+    return ((y_pred - y_true) ** 2).sum() / y_pred.size
+
 
 def ones(shape, requires_grad=False) -> Tensor:
     return Tensor(np.ones(shape), requires_grad=requires_grad)
@@ -277,17 +412,22 @@ def zeros(shape, requires_grad=False) -> Tensor:
 
 
 def rand(shape, requires_grad=False) -> Tensor:
-    return Tensor(np.random.rand(shape), requires_grad=requires_grad)
+    return Tensor(np.random.rand(*shape), requires_grad=requires_grad)
+
+
+def rand(num: int, requires_grad=False) -> Tensor:
+    return Tensor(np.arange(num), requires_grad=requires_grad)
 
 
 def tensor(*args, **kwargs):
     return Tensor(*args, **kwargs)
 
 
+def arange(*args, requires_grad=False):
+    return Tensor(np.arange(*args), requires_grad=requires_grad)
+
+
 if __name__ == "__main__":
-    x = Tensor([1, 2, 3], requires_grad=True)
-    y = Tensor([4], requires_grad=True)
-    z = x * y
-    z.backward(ones(z.shape))  # don't forget this
-    print(f"X: {x} grad: {x.grad}")
-    print(f"Y: {y} grad: {y.grad}")
+    x = arange(10, requires_grad=True)
+    x.sum().backward()
+    print(x.grad)
