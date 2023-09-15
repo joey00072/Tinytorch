softmax fix

gpt.py
@@ -10,8 +10,6 @@ import numpy as np
 import random
 import math
 
-from visulize import *
-
 # np.random.seed(0)
 # curl -LO https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 
@@ -106,19 +104,24 @@ class MHA(nn.Module):
         k = k.reshape(B, T, self.n_heads, C // self.n_heads)
         q = q.reshape(B, T, self.n_heads, C // self.n_heads)
         v = v.reshape(B, T, self.n_heads, C // self.n_heads)
+        print(f"{k.shape=}")
 
         k = k.transpose(1, 2)
         q = q.transpose(1, 2)
         v = v.transpose(1, 2)
 
         wei = (q @ k.transpose(-1, -2)) / (self.head_dim**0.5)
+
         wei = self.mask[:, :, :T, :T] + wei
+
         wei = F.softmax(wei, dim=-1)
+        print(wei[0][0])
 
         v = wei @ v
         v = v.transpose(1, 2).reshape(B, T, C)
 
         x = self.proj(v)
+        raise RuntimeError("STOP")
         return x
 
 
@@ -264,7 +267,7 @@ def train(model, optimizer: optim.Optimizer, dataset: TextDataset, num_iteration
 
         print(f"Iteration [{iteration + 1}/{num_iterations}], Loss: {loss.item()}")
 
-        if iteration % 10 == 0:
+        if iteration % 1000 == 0:
             generate(tokenizer, model)
         iteration += 1
         if iteration >= num_iterations:
@@ -277,7 +280,7 @@ def train(model, optimizer: optim.Optimizer, dataset: TextDataset, num_iteration
 # Define some hyperparameters
 learning_rate = 3e-3
 batch_size = 3
-seq_len = 10
+seq_len = 5
 d_model = 64
 n_heads = 2
 num_layers = 2
@@ -285,8 +288,6 @@ num_layers = 2
 tokenizer = Tokenizer("input.txt")
 train_data, val_data = tokenizer.train_val_split()
 
-batch_size = 8
-seq_len = 10
 
 train_dataset = TextDataset(train_data, batch_size, seq_len)
 val_dataset = TextDataset(val_data, batch_size, seq_len)

tinytorch.py
@@ -5,6 +5,8 @@ import numpy as np
 
 
 class Tensor:
+    __slots__ = ("data", "grad", "_ctx", "requires_grad")
+
     def __init__(self, data, requires_grad=False):
         self.data: np.ndarray = Tensor._data_to_numpy(data)
         self.grad: Tensor = None
@@ -24,7 +26,7 @@ class Tensor:
         raise ValueError("Invalid value passed to tensor")
 
     def __repr__(self) -> str:
-        s = f"{self.data} "
+        s = f"{np.round(self.data,4)} "
         if self.requires_grad:
             s += f" requires_grad={self.requires_grad}"
         if self._ctx is not None:
@@ -168,7 +170,7 @@ class Tensor:
         shape, new_shape = self._reduce_shape(axis)
         shape = shape if (shape) != () else (1,)
         ret: Tensor = Max.apply(self, new_shape)
-        return ret if keepdims else ret.reshape(shape)
+        return ret if keepdims else ret.reshape(shape), None
 
     def mean(x: Tensor, axis=None, keepdim=False):
         out = x.sum(axis=axis, keepdims=keepdim)
@@ -195,9 +197,14 @@ class Tensor:
     def _undo_broadcast(self, tensor: Tensor, grad: Tensor):
         data = tensor.data
         grad = grad.data
-        # print(f"{data.shape= },{grad.shape=}")
+
         while len(data.shape) != len(grad.shape):
             grad = grad.sum(axis=0, keepdims=(len(grad.shape) == 1))
+
+        for idx, (s1, s2) in enumerate(zip(data.shape, grad.shape)):
+            if s1 < s2:
+                grad = grad.sum(axis=idx, keepdims=True)
+
         return Tensor(grad)
 
     def backward(self, grad=None):
@@ -540,7 +547,8 @@ def sigmoid(x) -> Tensor:
 
 
 def softmax(x: Tensor, dim: int = 0) -> Tensor:
-    e_x = exp(x - x.max(axis=dim, keepdims=True))
+    m, _ = x.max(axis=dim, keepdims=True)
+    e_x = (x - m).exp()
     return e_x / e_x.sum(axis=dim, keepdims=True)
 
 
@@ -575,8 +583,8 @@ def rand(*shape, requires_grad=False) -> Tensor:
     return Tensor(np.random.rand(*shape), requires_grad=requires_grad)
 
 
-def tensor(*args, **kwargs):
-    return Tensor(*args, **kwargs)
+def tensor(data, requires_grad=False):
+    return Tensor(data, requires_grad)
 
 
 def arange(*args, requires_grad=False):